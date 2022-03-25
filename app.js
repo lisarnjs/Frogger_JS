a2e5a342dbe8cb1a91bd703383c65a41ac04a32b
권은서 <kwonenseo@gmail.com>
2022-03-25 17:19:13 +0900

    feat: Made lose & win & start/pause functon

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const timeLeftDisplay = document.querySelector('#time-left');
 const resultDisplay = document.querySelector('#result');
-const StartPauseButton = document.querySelector('#start-pause-button');
+const startPauseButton = document.querySelector('#start-pause-button');
 const squares = document.querySelectorAll('.grid div');
 const logsLeft = document.querySelectorAll('.log-left');
 const logsRight = document.querySelectorAll('.log-right');
@@ -9,6 +9,10 @@ const carsRight = document.querySelectorAll('.car-right');
 
 let currentIndex = 76;
 const WIDTH = 9;
+let timerId;
+let outcomeTimerId;
+let currentTime = 20;
+
 
 function moveFrog(e) {
   squares[currentIndex].classList.remove('frog');
@@ -31,13 +35,21 @@ function moveFrog(e) {
   squares[currentIndex].classList.add('frog');
 }
 
-document.addEventListener('keyup', moveFrog);
+
 
 function autoMoveElements() {
+  currentTime--;
+  timeLeftDisplay.textContent = currentTime;
   logsLeft.forEach(logLeft => moveLogLeft(logLeft));
   logsRight.forEach(logRight => moveLogRight(logRight));
   carsLeft.forEach(carLeft => moveCarLeft(carLeft));
   carsRight.forEach(carRight => moveCarRight(carRight));
+  
+}
+
+function checkOutComes() {
+  lose();
+  win();
 }
 
 function moveLogLeft(logLeft) {
@@ -124,5 +136,41 @@ function moveCarRight(carRight) {
   }
 }
 
+function lose() {
+  if (
+    squares[currentIndex].classList.contains('c1') || 
+    squares[currentIndex].classList.contains('l4') ||
+    squares[currentIndex].classList.contains('l5') ||
+    currentTime <= 0
+  ) {
+    resultDisplay.textContent = 'You Lose!';
+    clearInterval(timerId);
+    clearInterval(outcomeTimerId);
+    squares[currentIndex].classList.remove('frog');
+    document.removeEventListener('keyup', moveFrog);
+  }
+}
 
-setInterval(autoMoveElements, 1000)
+function win() {
+  if (squares[currentIndex].classList.contains('ending-block')) {
+    resultDisplay.textContent = 'You Win!';
+    clearInterval(timerId);
+    clearInterval(outcomeTimerId);
+    document.removeEventListener('keyup', moveFrog);
+  }
+}
+
+startPauseButton.addEventListener('click', () => {
+  if(timerId) {
+    clearInterval(timerId);
+    clearInterval(outcomeTimerId);
+    timerId = null;
+    outcomeTimerId = null;
+    document.removeEventListener('keyup', moveFrog);
+  } else {
+    timerId = setInterval(autoMoveElements, 1000);
+    outcomeTimerId = setInterval(checkOutComes, 50);
+    document.addEventListener('keyup', moveFrog);
+  }
+})
+
